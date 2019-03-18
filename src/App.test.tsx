6d387fb41962c9594723aa47754482c21eaeb05b
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2019-03-17 23:07:25 -0400

    noice

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { TestScheduler } from 'rxjs/testing';
 import { map } from 'rxjs/operators';
+import { zip } from 'rxjs';
 
-const scheduler = new TestScheduler((actual, expected) => {
+const objectEqualityTestScheduler = () => new TestScheduler((actual, expected) => {
   // asserting the two objects are equal
   expect(actual).toEqual(expected);
 });
@@ -17,7 +18,7 @@ it('renders without crashing', () => {
 
 it('should double source value', () => {
 
-  scheduler.run(helpers => {
+  objectEqualityTestScheduler().run(helpers => {
     const { cold, expectObservable } = helpers;
 
     const inputStream = cold('-a', { a: 5 });
@@ -30,3 +31,40 @@ it('should double source value', () => {
   });
 
 });
+
+describe('zip emits the most recently emitted item from each source observable, after all observables emit', () => {
+  it('no 3rd item will be emitted here', () => {
+    objectEqualityTestScheduler().run(({ cold, expectObservable} ) => {
+      const in1 = cold('--a--b--c')
+      const in2 = cold('d--e-----')
+
+      const outputStream = zip(in1, in2)
+
+      expectObservable(outputStream).toBe('--a--b---', { a: ['a','d'], b: ['b','e']})
+    })
+  })
+
+  it('also of note, order matters', () => {
+    objectEqualityTestScheduler().run(({ cold, expectObservable} ) => {
+      const in1 = cold('--a--b--c')
+      const in2 = cold('d--e-----')
+
+      const outputStream = zip(in2, in1)
+
+      expectObservable(outputStream).toBe('--a--b---', { a: ['d','a'], b: ['e','b']})
+    })
+  })
+
+  it('here is an interesting one with 3', () => {
+    objectEqualityTestScheduler().run(({ cold, expectObservable} ) => {
+      const in1 = cold('--a--b--c--')
+      const in2 = cold('d--e-------')
+      const in3 = cold('--------fgh')
+
+      const outputStream = zip(in1, in2, in3)
+
+      expectObservable(outputStream).toBe('--------fg-', { f: ['a','d', 'f'], g: ['b', 'e', 'g']})
+    })
+  })
+
+})
