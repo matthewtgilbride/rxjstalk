6421e1e6b741de640e3035a90259bd1fb7f5bf83
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2019-03-22 21:49:16 -0400

    rearranging things at last minute

--- src/test/concurrentMergeMap.test.ts
+++ src/test/concurrentMergeMap.test.ts
@@ -1,5 +1,5 @@
 import { objectEqualityTestScheduler } from "../testUtil";
-import { mergeMap } from "rxjs/operators";
+import { mergeMap, map } from "rxjs/operators";
 
 describe('the mergeMap diagram', () => {
 
@@ -160,6 +160,39 @@ describe('the mergeMap diagram', () => {
 
       const outputStream = in1$.pipe(
         mergeMap(() => in2$, (x, y) => "" + x + y, 2)
+      )
+
+      expectObservable(outputStream).toBe(out, values)
+
+    })
+  })
+
+  xit('extra time changes things?', () => {
+    objectEqualityTestScheduler().run(({cold, expectObservable}) => {
+
+      const in1 = 'a--- b--- c--- ---- ---- ---- ---- ---- ---- |'
+      const in2 = '1--- ---- ---- 2--- 3--- |'
+
+      const out = 'r--- s--- ---- t--- (uv) (wx) ---- ---- y--- z--- |'
+
+      const values = {
+        r: 'a1',
+        s: 'b1',
+        t: 'a2',
+        u: 'a3',
+        v: 'b2',
+        w: 'b3',
+        x: 'c1',
+        y: 'c2',
+        z: 'c3',
+      }
+
+
+      const in1$ = cold(in1)
+      const in2$ = cold(in2)
+
+      const outputStream = in1$.pipe(
+        mergeMap(x => in2$.pipe(map(y => x + y)), 2)
       )
 
       expectObservable(outputStream).toBe(out, values)
--- src/test/mergeMap.test.ts
+++ src/test/mergeMap.test.ts
@@ -1,23 +1,74 @@
-import { objectEqualityTestScheduler } from "../testUtil";
+import { objectEqualityTestScheduler, snapshotTestScheduler } from "../testUtil";
 import { mergeMap, map } from "rxjs/operators";
 
 
-describe('I <3 RxJS', () => {
-  it('mergeMap', () => {
-    objectEqualityTestScheduler().run(({cold, expectObservable}) => {
+describe('mergeMap', () => {
 
-      const in1 = 'a--- b--- c--- ---- ---- ---- ---- ---- ---- |'
-      const in2 = '1--- ---- ---- 2--- 3--- |'
+  it('plain mergeMap', () => {
+    objectEqualityTestScheduler().run(({ cold, expectObservable}) => {
 
-      const out = 'r--- s--- ---- t--- (uv) (wx) ---- ---- y--- z--- |'
+      const in1 = 'abc --- --- --- |'
+      const in2 = '1-- 2-- 3-- |'
+
+      const out = 'abc def ghi --- |'
 
       const values = {
-        r: 'a1',
-        s: 'b1',
-        t: 'a2',
+        a: 'a1',
+        b: 'b1',
+        c: 'c1',
+        d: 'a2',
+        e: 'b2',
+        f: 'c2',
+        g: 'a3',
+        h: 'b3',
+        i: 'c3'
+      }
+
+      const in1$ = cold(in1)
+      const in2$ = cold(in2)
+
+      const outputStream = in1$.pipe(
+        mergeMap((x) => in2$.pipe(map(y => `${x}${y}`))),
+      )
+
+      expectObservable(outputStream).toBe(out, values)
+
+    })
+  })
+
+  it('snapshot', () => {
+    snapshotTestScheduler().run(({cold, expectObservable}) => {
+
+      const in1 = 'abc --- --- --- --- |'
+      const in2 = '1-- 2-- 3-- |'
+
+
+      const in1$ = cold(in1)
+      const in2$ = cold(in2)
+
+      const outputStream = in1$.pipe(
+        mergeMap(x => in2$.pipe(map(y => x + y)), 2)
+      )
+
+      expectObservable(outputStream).toBe('')
+
+    })
+  })
+
+  it('equality', () => {
+    objectEqualityTestScheduler().run(({cold, expectObservable}) => {
+
+      const in1 = 'abc --- --- --- --- |'
+      const in2 = '1-- 2-- 3-- |'
+      const out = 'qr- st- uv- x-- y-- z-- |'
+
+      const values = {
+        q: 'a1',
+        r: 'b1',
+        s: 'a2',
+        t: 'b2',
         u: 'a3',
-        v: 'b2',
-        w: 'b3',
+        v: 'b3',
         x: 'c1',
         y: 'c2',
         z: 'c3',
